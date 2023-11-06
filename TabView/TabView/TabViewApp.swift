3a69cee8886fb54cbfdf10a663eff9917962f015
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-06 19:43:01 -0300

    Add files via upload

---
+++ TabView/TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Lucas Neves dos santos pompeu on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
